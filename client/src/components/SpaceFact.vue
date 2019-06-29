ae01b7295a74d3e438f5e219dc4f65b5f534c82a
cowang01 <g_cowan27@hotmail.com>
2019-06-29 17:30:46 +0100

    develop-v2

---
+++ client/src/components/SpaceFact.vue
@@ -0,0 +1,39 @@
+
+<template lang="html">
+  <div class="fact-box">
+    <p>test-fact_box</p>
+    <p>{{all}}</p>
+  </div>
+</template>
+
+<script>
+import SpaceServices from '@/services/SpaceServices.js'
+
+export default {
+  name: 'fact-box',
+  props: ['body'],
+  data() {
+    return {
+      all: [],
+      fact_list: [],
+      fact_group: 'all'
+    }
+  },
+  mounted(){
+    SpaceServices.getSpace()
+    .then( facts => this.all = facts);
+
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+  .fact-box {
+    border-style: solid;
+    border-color: black;
+    border-width: thick;
+    text-align: center;
+    align-items: center;
+  }
+</style>
