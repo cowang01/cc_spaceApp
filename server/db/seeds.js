feabd22a1822fd616301010a34e6fe57231ccd05
cowang01 <g_cowan27@hotmail.com>
2019-07-01 12:52:57 +0100

    monday_seedsFActs

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -119,41 +119,201 @@ db.spaceApp.insertMany([
   {
     type: "fact",
     name: "Mercury",
-    fact: "Mercury is amazing"
+    fact: "Mercury is the smallest planet in our solar system"
   },
   {
     type: "fact",
     name: "Mercury",
-    fact: "Mercury was named after Freddy Mercury"
-  },
-  {
-    type: "fact",
-    name: "Earth",
-    fact: "Earth is where you live"
+    fact: "Your weight on Mercury would be 38% of your weight on Earth"
   },
   {
     type: "fact",
     name: "Mercury",
-    fact: "This planet is really hot!!!"
+    fact: "It’s not known who discovered Mercury"
+  },
+  {
+    type: "fact",
+    name: "Mercury",
+    fact: "Mercury is the most cratered planet in the Solar System"
+  },
+  {
+    type: "fact",
+    name: "Venus",
+    fact: "The surface temperature on Venus can reach 471 °C."
+  },
+  {
+    type: "fact",
+    name: "Venus",
+    fact: "Venus is thought to be made up of a central iron core, rocky mantle and silicate crust"
+  },
+  {
+    type: "fact",
+    name: "Venus",
+    fact: "Venus rotates in the opposite direction to most other planets"
+  },
+  {
+    type: "fact",
+    name: "Venus",
+    fact: "Venus is the second brightest object in the night sky"
   },
   {
     type: "fact",
     name: "Earth",
-    fact: "This planet has the highest population of living creatures know to man, in fact Earth is the ONLY planet we know of capable of supporting our life!"
+    fact: "The Earth’s rotation is gradually slowing"
+  },
+  {
+    type: "fact",
+    name: "Earth",
+    fact: "The Earth was once believed to be the centre of the universe"
+  },
+  {
+    type: "fact",
+    name: "Earth",
+    fact: "Earth is the only planet not named after a god"
+  },
+  {
+    type: "fact",
+    name: "Earth",
+    fact: "Our planet is the only known planet to support life"
+  },
+  {
+    type: "fact",
+    name: "Mars",
+    fact: "Mars and Earth have approximately the same landmass"
+  },
+  {
+    type: "fact",
+    name: "Mars",
+    fact: "Mars is home to the tallest mountain in the solar system"
+  },
+  {
+    type: "fact",
+    name: "Mars",
+    fact: "Pieces of Mars have fallen to Earth"
+  },
+  {
+    type: "fact",
+    name: "Mars",
+    fact: "There are signs of liquid water on Mars"
+  },
+  {
+    type: "fact",
+    name: "Jupiter",
+    fact: "Jupiter has the shortest day of all the planets"
+  },
+  {
+    type: "fact",
+    name: "Jupiter",
+    fact: "Jupiter’s interior is made of rock, metal, and hydrogen compounds"
+  },
+  {
+    type: "fact",
+    name: "Jupiter",
+    fact: "Jupiter’s moon Ganymede is the largest moon in the solar system"
+  },
+  {
+    type: "fact",
+    name: "Jupiter",
+    fact: "Eight spacecraft have visited Jupiter"
   },
   {
     type: "fact",
     name: "Saturn",
-    fact: "I like Saturn"
+    fact: "Saturn is the flattest planet"
+  },
+  {
+    type: "fact",
+    name: "Saturn",
+    fact: "Saturn’s upper atmosphere is divided into bands of clouds"
+  },
+  {
+    type: "fact",
+    name: "Saturn",
+    fact: "Saturn is made mostly of hydrogen"
+  },
+  {
+    type: "fact",
+    name: "Saturn",
+    fact: "Saturn can be seen from Earth with the naked eye"
+  },
+  {
+    type: "fact",
+    name: "Uranus",
+    fact: "Uranus was officially discovered by Sir William Herschel in 1781"
+  },
+  {
+    type: "fact",
+    name: "Uranus",
+    fact: "Uranus is often referred to as an “ice giant” planet"
+  },
+  {
+    type: "fact",
+    name: "Uranus",
+    fact: "Uranus hits the coldest temperatures of any planet"
+  },
+  {
+    type: "fact",
+    name: "Uranus",
+    fact: "Uranus has two sets of very thin dark coloured rings"
   },
   {
     type: "fact",
     name: "Neptune",
-    fact: "WHOOO NEPTUNE!!"
+    fact: "Only one spacecraft has flown by Neptune"
   },
   {
     type: "fact",
     name: "Neptune",
-    fact: "If you look out the window now you will see Neptune"
+    fact: "Neptune has a very thin collection of rings"
+  },
+  {
+    type: "fact",
+    name: "Neptune",
+    fact: "The atmosphere of Neptune is made of hydrogen and helium, with some methane"
+  },
+  {
+    type: "fact",
+    name: "Neptune",
+    fact: "Neptune spins on its axis very rapidly"
+  },
+  {
+    type: "fact",
+    name: "Pluto",
+    fact: "Pluto is named after the Roman god of the underworld"
+  },
+  {
+    type: "fact",
+    name: "Pluto",
+    fact: "Pluto was reclassified from a planet to a dwarf planet in 2006."
+  },
+  {
+    type: "fact",
+    name: "Pluto",
+    fact: "Pluto was discovered on February 18th, 1930 by the Lowell Observatory"
+  },
+  {
+    type: "fact",
+    name: "Pluto",
+    fact: "Pluto is one third water"
+  },
+  {
+    type: "fact",
+    name: "Sun",
+    fact: "One million Earths could fit inside the Sun"
+  },
+  {
+    type: "fact",
+    name: "Sun",
+    fact: "The Sun will one day be about the size of Earth"
+  },
+  {
+    type: "fact",
+    name: "Sun",
+    fact: "The Sun is composed of hydrogen (70%) and Helium (28%)"
+  },
+  {
+    type: "fact",
+    name: "Sun",
+    fact: "The Sun travels at 220 kilometres per second"
   }
 ]);
