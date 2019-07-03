6d2ebf8a3469cf06688db60a247727d16c8d50e8
cowang01 <g_cowan27@hotmail.com>
2019-07-03 11:11:55 +0100

    wednesday_question

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Ether from '@/views/Ether';
+import Question1 from '@/views/Question1.vue'
 
 
 Vue.use(Router);
@@ -12,6 +13,11 @@ const router = new Router({
       path: '',
       name: 'ether',
       component: Ether
+    },
+    {
+      path: '/question',
+      name: 'question1',
+      component: Question1
     }
   ]
 })
